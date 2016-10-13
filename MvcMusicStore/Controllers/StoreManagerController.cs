5c807fa8e2a41b59180756ae3a30e451f34558a3
xqlcrystal <xql252660@163.com>
2016-10-13 15:08:00 +0800

    add scripts

---
+++ MvcMusicStore/Controllers/StoreManagerController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcMusicStore.Models;
+
+
+namespace MvcMusicStore.Controllers
+{
+    public class StoreManagerController : Controller
+    {
+        MusicStoreEntities storeDB = new MusicStoreEntities();
+
+        //
+        // GET: /StoreManager/
+
+        public ActionResult Index()
+        {
+            var albums = storeDB.Albums.Include("Genre").Include("Artist");
+            return View(albums.ToList<Album>());
+        }
+
+        // GET: /StoreManager/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            Album album = storeDB.Albums.Find(id);
+            ViewBag.GenreId = new SelectList(storeDB.Genres, "GenreId", "Name", album.AlbumId);
+            ViewBag.ArtistId = new SelectList(storeDB.Artist, "ArtistId", "Name", album.ArtistId);
+            return View(album);
+
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Album album)
+        {
+            if(ModelState.IsValid)
+            {
+                storeDB.Entry(album).State = System.Data.Entity.EntityState.Modified;
+                storeDB.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.GenreId = new SelectList(storeDB.Genres, "GenreId", "Name", album.AlbumId);
+            ViewBag.ArtistId = new SelectList(storeDB.Artist, "ArtistId", "Name", album.ArtistId);
+            return View(album);
+        
+
+        }
+
+        // GET: /StoreManager/Details/5
+        public ActionResult Details(int id)
+        {
+            Album album = storeDB.Albums.Find(id);
+            return View(album);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Album album = storeDB.Albums.Find(id);
+            return View(album);
+        }
+
+
+        [HttpPost,ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Album album = storeDB.Albums.Find(id);
+            storeDB.Albums.Remove(album);
+            storeDB.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //[HttpPost]
+        //public ActionResult Delete(int id, FormCollection collection)
+        //{
+        //    try
+        //    {
+        //          Album album = storeDB.Albums.Find(id);
+        //          storeDB.Albums.Remove(album);
+        //          return RedirectToAction("Index");
+        //    }
+        //    catch (Exception)
+        //    {
+
+        //        return View();
+        //    }
+        //}
+
+        public ActionResult Create()
+        {
+            ViewBag.GenreId = new SelectList(storeDB.Genres, "GenreId", "Name");
+            ViewBag.ArtistId = new SelectList(storeDB.Artist, "ArtistId", "Name");
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(Album album)
+        {
+            if (ModelState.IsValid)
+            {
+                storeDB.Albums.Add(album);
+                storeDB.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.GenreId = new SelectList(storeDB.Genres, "GenreId", "Name",album.AlbumId);
+            ViewBag.ArtistId = new SelectList(storeDB.Artist, "ArtistId", "Name",album.ArtistId);
+            return View();
+        }
+    }
+}
--- MvcMusicStore/Models/Album.cs
+++ MvcMusicStore/Models/Album.cs
@@ -2,18 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using System.ComponentModel;
 
 namespace MvcMusicStore.Models
 {
+    [Bind(Exclude = "AlbumId")]
     public class Album
     {
+        [ScaffoldColumn(false)]
         public int AlbumId { get; set; }
+
+        [DisplayName("Genre")]
         public int GenreId { get; set; }
+
+         [DisplayName("Artist")]
         public int ArtistId { get; set; }
+
+        [Required(ErrorMessage="An Album title is required")]
+        [StringLength(160)]
         public string Title { get; set; }
+
+        [Required(ErrorMessage="Price is required")]
+        [Range(0.01,100.00,ErrorMessage="Price must be between 0.0.1 and 100.00")]
         public decimal Price { get; set; }
+
+        [DisplayName("Album Art URL")]
         public string AlbumArtUrl { get; set; }
-        public Genre Genre { get; set; }
-        public Artist Artist { get; set; }
+        public virtual Genre Genre { get; set; }
+        public virtual Artist Artist { get; set; }
     }
 }
